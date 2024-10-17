2a32543767749324bfe42e9e93f1e79b836a048d
Hellion <semakin.s.v@gmail.com>
2024-10-18 01:17:41 +0300

    app done

--- app/src/main/java/ru/urban/android_trafficlaws/MainActivity.kt
+++ app/src/main/java/ru/urban/android_trafficlaws/MainActivity.kt
@@ -1,12 +1,19 @@
 package ru.urban.android_trafficlaws
 
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var checkBox: CheckBox
+
+    private lateinit var infoTextView: TextView
+    private lateinit var lawsTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +22,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        infoTextView = findViewById(R.id.infoViewTV)
+        lawsTextView = findViewById(R.id.lawsViewTV)
+
+        checkBox = findViewById(R.id.checkBoxCB)
+
+        checkBox.setOnCheckedChangeListener{button, isChecked ->
+            if (isChecked){
+                infoTextView.text = "Правила дорожного движения"
+                lawsTextView.text = TrafficLaws().laws
+            } else {
+                infoTextView.text = "Информация"
+                lawsTextView.setText("")
+            }
         }
     }
 }
